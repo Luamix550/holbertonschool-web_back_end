fb182577bd70f2242c77d0c44dfdb8cac57afec8
Luamix <luizalfonso611@gmail.com>
2024-05-08 17:03:50 -0500

    Done all task

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
+
+from typing import List
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    data = [task_wait_random(max_delay) for _ in range(n)]
+    data = await asyncio.gather(*data)
+    return sorted(data)
