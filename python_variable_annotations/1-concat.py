679fda288c6b6612c36a557c14459f85aba22007
Luamix <luizalfonso611@gmail.com>
2024-05-08 15:24:58 -0500

    Fix pycode

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,4 +5,13 @@ Concat two strings
 
 
 def concat(str1: str, str2: str) -> str:
+    """concat two strings
+
+    Args:
+        str1 (str)
+        str2 (str)
+
+    Returns:
+        str: str concat
+    """
     return str1 + str2
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,5 +3,15 @@
 type-annotated function floor which takes a 
 float n as argument and returns the floor of the float.
 """
+
+
 def floor(n: float) -> float:
+    """_summary_
+
+    Args:
+        n (float): Arg
+
+    Returns:
+        float: _description_
+    """
     return n
