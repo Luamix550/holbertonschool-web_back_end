404c8561b9a02051930c0377cc5dec6077eb3d7f
Luamix <luizalfonso611@gmail.com>
2024-05-08 16:27:05 -0500

    Change type return

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """
-function to_str that takes a float n as argument 
+function to_str that takes a float n as argument
 and returns the string representation of the float.
 """
+
+
 def to_str(n: float) -> str:
     """
 
     Args:
-        n (float): 
+        n (float)
 
     Returns:
-        str: _description_
+        str: string representation
     """
     return str(n)
