7d54713141873cd3c89c74a6c716e81e99e6d180
Luamix <luizalfonso611@gmail.com>
2024-05-14 11:23:01 -0500

    Task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Python function that changes all topics of a school
+document based on the name
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """Update all topics based on the name
+
+    Args:
+        mongo_collection: Collection to change topics 
+        name: filter to cahnge the topic
+        topics: to change
+
+    Returns:
+        return changes
+    """
+    update_name = {name: "name"}
+    update_topics = {"$set": {topics: "topics"}}
+    result = mongo_collection.updateOne(update_name, update_topics)
+    return result
