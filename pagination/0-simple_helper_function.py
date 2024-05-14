290db0fb9225dbe18c0506a45f384e1f3aee02b3
Luamix <luizalfonso611@gmail.com>
2024-05-14 13:40:52 -0500

    Task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,7 @@ for those particular pagination parameters.
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size - 1
