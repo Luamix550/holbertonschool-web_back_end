3a2495773eed7d355061e98024e157128e13c940
Luamix <luizalfonso611@gmail.com>
2024-05-14 11:05:21 -0500

    Task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Python function that inserts a new document in
+a collection based on kwargs
+"""
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    insert_d = mongo_collection.insert_many(kwargs)
+    return insert_d.inserted_id
