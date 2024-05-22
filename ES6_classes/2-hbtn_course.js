9690246429247b2d1961836da7f73d7074bb9183
Luamix <luizalfonso611@gmail.com>
2024-05-22 17:33:57 -0500

    Task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,22 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
-
-    if (Array.isArray(students)) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array');
-    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
