37c7a8170bf8f5ab68cae2de63d0bb64962526cf
Luamix <luizalfonso611@gmail.com>
2024-05-23 13:36:30 -0500

    Task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,34 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    if (this.constructor === Building) {
+      throw new Error('You cannot instantiate an abstract class!');
     }
-    this._sqft = sqft;
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('The class extending Building must override the method evacuationWarningMessage');
+    }
+
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new Error('Square footage must be a number.');
+    }
   }
 
   get sqft() {
-    return this.sqft;
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value === 'number') {
+      this._sqft = value;
+    } else {
+      throw new TypeError('Square footage must be a number.');
+    }
   }
 
   /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error("Method 'evacuationWarningMessage()' must be implemented");
   }
-  /* eslint-disable class-methods-use-this */
+  /* eslint-enable class-methods-use-this */
 }
