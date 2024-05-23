55c0c2e145eb7e16040aaea55817f85045712a77
Luamix <luizalfonso611@gmail.com>
2024-05-23 15:44:18 -0500

    Task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,13 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    } else {
       this._sqft = sqft;
+    } else if (!this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-    /* eslint-disable class-methods-use-this */
-  }
-  /* eslint-disable class-methods-use-this */
 }
