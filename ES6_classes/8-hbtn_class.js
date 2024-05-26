7ea5041ca68594f0666e11b95320890e9ba42000
Luamix <luizalfonso611@gmail.com>
2024-05-26 12:38:39 -0500

    Task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  theString() {
+    return this._size;
+  }
+
+  thelocation() {
+    return this.location;
+  }
+}
