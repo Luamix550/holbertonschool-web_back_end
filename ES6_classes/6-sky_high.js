f3d7be58067e6a17ed4ba2f9e65be88d9b389b6b
Luamix <luizalfonso611@gmail.com>
2024-05-26 12:01:15 -0500

    Task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,9 +1,13 @@
-import Building from './5-building';
+import { Building } from './5-building';
 
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
         super(sqft);
+        if (typeof floors === 'number'){
         this._floors = floors;
+        } else {
+            throw TypeError("floors must be a number")
+        }
     }
 
     get sqft() {
