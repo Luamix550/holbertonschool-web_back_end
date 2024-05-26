fd7424553b0b9e4c58e23751c8f3c8afe3b3dec6
Luamix <luizalfonso611@gmail.com>
2024-05-26 12:04:34 -0500

    Task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,24 +1,24 @@
-import { Building } from './5-building';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        if (typeof floors === 'number'){
-        this._floors = floors;
-        } else {
-            throw TypeError("floors must be a number")
-        }
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors === 'number') {
+      this._floors = floors;
+    } else {
+      throw TypeError('floors must be a number');
     }
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evalatuationMethodWarning (){
-        return `Evacuate slowly the ${this._floors} floors`
-    }
+  evalatuationMethodWarning() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
