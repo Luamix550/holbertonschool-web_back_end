3e3ebea33fa0d2e635e4ea2210dbc4f035d8f886
Luamix <luizalfonso611@gmail.com>
2024-06-01 18:28:22 -0500

    Task 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+   try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+   } catch (err) {
+    queue.push(`${err.name}: ${err.message}`);
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
