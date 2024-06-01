4d4d64daa3d824799dd13c88c705e011cb2555e5
Luamix <luizalfonso611@gmail.com>
2024-06-01 18:20:17 -0500

    Task 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,4 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator > 0) return numerator / denominator;
+  if (denominator > 0) return numerator / denominator;
     throw new Error('cannot divide by 0');
   }
+  
