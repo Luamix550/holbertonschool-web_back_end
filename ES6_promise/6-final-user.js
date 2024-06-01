9eb302d07015357e813c0ff16e949e0e8f3eae0c
Luamix <luizalfonso611@gmail.com>
2024-06-01 17:52:13 -0500

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,14 @@ import signUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ])
-  .then((promise) => {
-    return promise.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : `${result.reason}`
-    }));
-  });
+    return Promise.allSettled([
+        signUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ])
+    .then((promise) => {
+        return promise.map((result) => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
+        }));
+    });
 }
