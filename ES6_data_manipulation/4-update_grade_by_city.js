b2a3b1de999da49f857105a448ae9ad0a5b94404
Luamix <luizalfonso611@gmail.com>
2024-06-04 15:44:11 -0500

    Task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const filteredStudents = students.filter(student => student.location === city);
+
+  const updatedStudents = filteredStudents.map(student => {
+  const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+
+  const newGrade = gradeObj ? gradeObj.grade : 'N/A';
+    return {
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: newGrade
+    };
+  });
+return updatedStudents;
+}
