d0926e792a669b5ccffccc7031854a38888ee098
Luamix <luizalfonso611@gmail.com>
2024-06-05 17:05:15 -0500

    Task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(itemMap) {
   if (!(itemMap instanceof Map)) {
-    throw new Error('Cannot process')
+    throw new Error('Cannot process');
   }
   itemMap.forEach((item, quantity) => {
     if (item === 1) {
-      itemMap.set(quantity, 100)
-   }
+      itemMap.set(quantity, 100);
+    }
   });
-return itemMap;
+  return itemMap;
 }
