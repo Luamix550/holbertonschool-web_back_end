811455047295e2331175eee77749b1d9a1e7faef
Luamix <luizalfonso611@gmail.com>
2024-06-05 13:34:12 -0500

    Task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
 export default function createInt8TypedArray (length, position, value) {
-    const buffer = new ArrayBuffer(length);    
+  const buffer = new ArrayBuffer(length);    
 
-    if (position < 0 || position > buffer.length)
-      throw new Error('Position outside range');
+  if (position < 0 || position > buffer.length) {
+    throw new Error('Position outside range');
+  }
 
-    const data = new DataView(buffer);
-    data.setInt8(position, value);
+  const data = new DataView(buffer);
+  data.setInt8(position, value);
     
-    return data;
+  return data;
 }
