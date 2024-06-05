b6922f8d94a72594f94206bfdb5cd3a65815bb79
Luamix <luizalfonso611@gmail.com>
2024-06-05 14:52:21 -0500

    Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(arrObj) {
   if (!Array.isArray(arrObj)) {
     return [];
-}
-  return arrObj.map(item => item.id);
+  }
+  return arrObj.map((item) => item.id);
 }
