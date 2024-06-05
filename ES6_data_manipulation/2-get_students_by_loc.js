42bb94f95172897c31252f6a0ade33cde8bca55b
Luamix <luizalfonso611@gmail.com>
2024-06-05 14:56:40 -0500

    Task 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(studentArray, city) {
-  return studentArray.filter(student => student.location === city)
+  return studentArray.filter((student) => student.location === city)
 }
