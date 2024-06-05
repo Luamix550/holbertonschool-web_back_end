7e1edf8564a3b1a61b9bbed41b0212a402f444a8
Luamix <luizalfonso611@gmail.com>
2024-06-05 16:18:33 -0500

    Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString){
+  if (!startString) {
     return '';
   }
   const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       result.push(value.slice(startString.length));
-    }
+  }
 });
   return result.join('-');
 }
